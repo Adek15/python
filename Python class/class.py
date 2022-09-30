3daa070ded22f2f46c7b11065cf4fb63c4737866
Adek15 <88534016+Adek15@users.noreply.github.com>
2022-09-30 08:06:46 +0100

    first commit

---
+++ Python class/class.py
@@ -0,0 +1,17 @@
+# price = float(input("Enter the price: "))
+# if (price > 10000):
+#     discount_price = price*0.2
+#     amount= price-discount_price
+#     print('The amount to be paid is: '+ str(amount))
+
+
+# for N in range (1, 101):
+#     if N%2==0:
+#         print(N)
+
+# x = int(input('Enter value of x: '))
+# y = int(input('Enter value of y: '))
+
+# z = x**2 + y**2
+
+# print(z)
